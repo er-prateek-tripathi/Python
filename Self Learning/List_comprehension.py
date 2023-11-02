801afa33b7f5f9c4a72b2efe64599aedc0dfd46c
PREDATOR <prateek772492@gmail.com>
2023-11-02 13:38:29 +0530

    List and various usage of it

---
+++ Self Learning/List_comprehension.py
@@ -0,0 +1,36 @@
+# list = used to store multiple items in single variable
+# it can be updated and changed any time.
+food = ["pizza", "chaat", "burger", "paneer-tikka"]
+
+food[0] = "Chowmein"
+
+print(food)
+
+# for loop
+for x in food:
+    print(x)
+
+# adding element in a list
+food.append("ice-cream")
+print(food)
+
+# removing desired element from list
+food.remove("burger")
+print(food)
+
+# popping is same as removing, but it pos from the end of list
+food.pop()
+print(food)
+
+# inserting an element at desired index
+food.insert(0, "Cake")
+print(food)
+
+# sorting the list
+food.sort()
+print(food)
+
+# deleting all the elements from the list, except the list itself
+food.clear()
+print(food)
+
